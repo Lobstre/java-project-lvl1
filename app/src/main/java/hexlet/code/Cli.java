1f2cf23b3a9fd96a80a864e54b845e8b45bb2270
ilya yaranov <di-we@outlook.com>
2022-11-18 00:51:33 +0300

    Step 9

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -31,7 +31,7 @@ public class Cli {
 
     public static void gameNum() {
         boolean found = false;
-        String[] gameNums = new String[]{"1", "2", "3", "4", "5", "6"};
+        String[] gameNumbers = new String[]{"1", "2", "3", "4", "5", "6"};
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("""
@@ -44,7 +44,7 @@ public class Cli {
                 0 - Exit""");
         System.out.print("Your choice: ");
         String gameNum = scanner.nextLine();
-        for (String i : gameNums) {
+        for (String i : gameNumbers) {
             if (i.equals(gameNum)) {
                 found = true;
                 break;
