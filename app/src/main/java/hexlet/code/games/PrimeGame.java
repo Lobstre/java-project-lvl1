fb6224ea138c9ff6bbe22dbf7612a42a36de591f
ilya yaranov <di-we@outlook.com>
2022-11-21 23:05:53 +0300

    Step 9 refactoring

--- app/src/main/java/hexlet/code/games/PrimeGame.java
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -2,7 +2,7 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 public class PrimeGame {
-    public static int firstNotPrime = 4;
+    private static final int firstNotPrime = 4;
     public static void primeGame() {
         String[][] qAndAs = new String[Engine.STAGE][2];
         String question = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
@@ -11,7 +11,6 @@ public class PrimeGame {
             qAndAs[i][0] = String.valueOf(result);
             qAndAs[i][1] = isPrime(result) ? "yes" : "no";
         }
-
         Engine.braingamesEngine(qAndAs, question);
     }
 
